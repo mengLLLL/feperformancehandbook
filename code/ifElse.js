bb724708d043fc811e7a92c4070b81bd16816335
mengliang <mengliang@yzsci.cn>
2019-01-16 13:33:31 +0800

    add switch test

--- code/ifElse.js
+++ code/ifElse.js
@@ -1,5 +1,5 @@
 
-function howManyDays(month){
+function ifElse(month){
     if(month === 1 ||
         month === 3 ||
         month === 5 ||
@@ -16,7 +16,7 @@ function howManyDays(month){
     }
 }
 
-function howManyDays2(month){
+function objectType(month){
     const table = {
         1: 31, 3: 31, 5: 31, 7: 31, 8: 31, 10: 31, 12:31,
         4: 30, 6:30, 9: 30, 11: 30,
@@ -25,16 +25,44 @@ function howManyDays2(month){
     return table[month]
 }
 
-function howManyDays3(month) {
-    const monthObject = {
-        1: 31, 3: 31, 5: 31, 7: 31, 8: 31, 10: 31, 12:31,
-        4: 30, 6:30, 9: 30, 11: 30,
-        2: 29
+
+const objectFntype = {
+    1: () => { return 31 },
+    2: () => { return 29 },
+    3: () => { return 31 },
+    4: () => { return 30 },
+    5: () => { return 31 },
+    6: () => { return 30 },
+    7: () => { return 31 },
+    8: () => { return 31 },
+    9: () => { return 30 },
+    10: () => { return 31 },
+    11: () => { return 30 },
+    12: () => { return 31 },
+}
+
+function switchCase(month) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 31;
+        case 2:
+            return 29
     }
-    return monthObject[month]
 }
 module.exports = {
-    howManyDays,
-    howManyDays2,
-    howManyDays3
+    ifElse,
+    objectType,
+    switchCase,
+    objectFntype
 }
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var Benchmark = require('benchmark');
-
+var suite = new Benchmark.Suite
 var ifElse = require('./code/ifElse')
 
-var bench = new Benchmark(
-    'tableTest',
-    ifElse.howManyDays3,
-)
+suite.add('ifElse', function() {
+    ifElse.ifElse()
+}).add('objectType', function() {
+    ifElse.objectType()
+}).add('switchCase', function() {
+    ifElse.switchCase()
+}).on('cycle', function(event) {
+    console.log(String(event.target))
+}).run({'async': true})
 
+// bench.run() // 开始测试
 
-bench.run() // 开始测试
-
-console.log(bench.hz) // 每秒运行数
-console.log(bench.stats.moe) // 出错边界
-console.log(bench.stats.variance) // 样本方差
+// console.log(bench.hz) // 每秒运行数
+// console.log(bench.stats.moe) // 出错边界
+// console.log(bench.stats.variance) // 样本方差
