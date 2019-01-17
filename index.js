200cd8741d7f7b7343559e17b518bffca1d047e9
wanghl <alun@yzsci.cn>
2019-01-17 11:56:14 +0800

    三元运算符和if...else的比较

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var Benchmark = require('benchmark')
 var ifElse = require('./code/ifElse')
 var testFor = require('./code/for')
 var array = require('./code/array')
 var spliceString = require('./code/spliceString')
+var ternaryOperator = require('./code/ternaryOperator')
 
-array.testDel()
+ternaryOperator.testTernary()
